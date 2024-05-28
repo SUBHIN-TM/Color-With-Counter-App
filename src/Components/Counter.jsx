a90835d4004466d116b59d2d8733ccbe6bef36e6
SUBHIN-TM <subhintm2018@gmail.com>
2024-05-28 15:14:15 +0530

    audio

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -1,73 +1,87 @@
 import { useSelector, useDispatch } from 'react-redux'
-import { decrement,increment,reset,customValue } from '../features/counter/counterSlice'
-import { useState } from 'react'
-import userColor from '../hooks/useColor';
+import { decrement, increment, reset, customValue } from '../features/counter/counterSlice'
+import { useState, useEffect } from 'react'
+import userColor from '../hooks/useColor'
+import annyang from 'annyang'
 
+const Counter = () => {
+  const count = useSelector((state) => state.counter.value)
+  const error = useSelector((state) => state.counter.error)
+  const [value, setValue] = useState('')
+  const dispatch = useDispatch()
 
-
-const Counter=()=>{
- const count=useSelector((state)=>state.counter.value)
- const error = useSelector((state) => state.counter.error)
- const [value,setValue]=useState('')
- const dispatch=useDispatch()
-
- const applyCustomValue=()=>{
-  dispatch(customValue(Number(value)))
- }
-
-
-
- const getColorClass = () => {
- 
-
-  if (count >= 1 && count <= 10) {
-    return userColor(count,"red")
-  } else if (count >= 11 && count <= 20) {
-    return userColor(count,"green")
-  } else if (count >= 21 && count <= 30) {
-    return userColor(count,"yellow")
-  }else{
-    return ""
+  const applyCustomValue = () => {
+    dispatch(customValue(Number(value)))
   }
-};
 
-const resetFunction=()=>{
-  setValue('')
-  dispatch(reset())
-}
+  const getColorClass = () => {
+    if (count >= 1 && count <= 10) {
+      return userColor(count, "red")
+    } else if (count >= 11 && count <= 20) {
+      return userColor(count, "green")
+    } else if (count >= 21 && count <= 30) {
+      return userColor(count, "yellow")
+    } else {
+      return ""
+    }
+  }
 
- return(
+  const resetFunction = () => {
+    setValue('')
+    dispatch(reset())
+  }
+
+  useEffect(() => {
+    if (annyang) {
+      const commands = {
+        'reset': () => {
+          resetFunction()
+        },
+        'input *num': (num) => {
+          dispatch(customValue(Number(num)))
+        }
+      }
+
+      annyang.addCommands(commands)
+      annyang.start()
+      console.log("voice start")
+
+      return () => {
+        annyang.abort()
+      }
+    }
+  }, [dispatch])
+
+  return (
     <>
-    <div className='outLine m-auto' >
-      <div className={`innerLine colorChange ${getColorClass()}`} >
-        <div className='valueBox m-auto mt-6 font-bold text-3xl rounded-lg bg-green' >{count}</div> 
-        {error && (<div className='mt-3 bg-white w-80 m-auto '> <h1 className='text-red-600'>Invalid Value</h1></div>)}   
-        <div className='flex justify-around mt-11'>
-          <button onClick={()=>dispatch(decrement())} className='border p-2  bg-black  text-white font-semibold'> ➖ Decrement </button>
-          <button onClick={()=>dispatch(increment(1))} className='border p-2 bg-black  text-white font-semibold'>➕ Increment</button>
-        </div>
-        <div className='mt-10 ' >
-        <label className='font-semibold' htmlFor="value">Custom Value :</label>  <input onChange={(e)=>setValue(e.target.value)} value={value} className='border border-black w-40' type="number" name="" id="" />
-        </div>
-        <button onClick={applyCustomValue} className='border p-1 bg-black text-white font-semibold mt-2 ml-28 px-3'>Apply</button> <button onClick={()=>resetFunction()} className='border p-1 bg-black text-white font-semibold mt-2 ml-5 px-3'>Reset</button>
-        <div className='border mt-4 p-10 bg-black'>
-          <div className={`border  colorChange ${getColorClass()}`}> 
-          <h1 className='font-bold p-1'>VALUE RANGES</h1>
-          <div className='flex flex-col pb-4'>
-          <label>Red : 1 - 10</label>
-          <label>Green : 11 - 20</label>
-          <label>Yellow : 21 - 30</label>
+      <div className='outLine m-auto'>
+        <div className={`innerLine colorChange ${getColorClass()}`}>
+          <div className='valueBox m-auto mt-6 font-bold text-3xl rounded-lg bg-green'>{count}</div>
+          {error && (<div className='mt-3 bg-white w-80 m-auto '> <h1 className='text-red-600'>Invalid Value</h1></div>)}
+          <div className='flex justify-around mt-11'>
+            <button onClick={() => dispatch(decrement())} className='border p-2  bg-black  text-white font-semibold'> ➖ Decrement </button>
+            <button onClick={() => dispatch(increment(1))} className='border p-2 bg-black  text-white font-semibold'>➕ Increment</button>
           </div>
+          <div className='mt-10'>
+            <label className='font-semibold' htmlFor="value">Custom Value :</label>
+            <input onChange={(e) => setValue(e.target.value)} value={value} className='border border-black w-40' type="number" name="" id="" />
+          </div>
+          <button onClick={applyCustomValue} className='border p-1 bg-black text-white font-semibold mt-2 ml-28 px-3'>Apply</button>
+          <button onClick={() => resetFunction()} className='border p-1 bg-black text-white font-semibold mt-2 ml-5 px-3'>Reset</button>
+          <div className='border mt-4 p-10 bg-black'>
+            <div className={`border colorChange ${getColorClass()}`}>
+              <h1 className='font-bold p-1'>VALUE RANGES</h1>
+              <div className='flex flex-col pb-4'>
+                <label>Red : 1 - 10</label>
+                <label>Green : 11 - 20</label>
+                <label>Yellow : 21 - 30</label>
+              </div>
+            </div>
           </div>
         </div>
-      </div>    
-    </div>
+      </div>
     </>
- )
-
-
+  )
 }
 
-
-export default Counter;
-
+export default Counter
