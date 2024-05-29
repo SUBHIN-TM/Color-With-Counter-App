e377b78f5da389cf429ee3452c1ac1ea821a01c5
SUBHIN-TM <subhintm2018@gmail.com>
2024-05-29 12:14:32 +0530

    old technique

--- src/Components/Counter.jsx
+++ src/Components/Counter.jsx
@@ -58,10 +58,45 @@ const Counter = () => {
     }
   }, [dispatch])
 
+const colorSelect={
+  1:'bg-red-100',
+  2:'bg-red-200',
+  3:'bg-red-300',
+  4:'bg-red-400',
+  5:'bg-red-500',
+  6:'bg-red-600',
+  7:'bg-red-700',
+  8:'bg-red-800',
+  9:'bg-red-900',
+  10:'bg-red-950',
+  11:'bg-green-100',
+  12:'bg-green-200',
+  13:'bg-green-300',
+  14:'bg-green-400',
+  15:'bg-green-500',
+  16:'bg-green-600',
+  17:'bg-green-700',
+  18:'bg-green-800',
+  19:'bg-green-900',
+  20:'bg-green-950',
+  21:'bg-yellow-100',
+  22:'bg-yellow-200',
+  23:'bg-yellow-300',
+  24:'bg-yellow-400',
+  25:'bg-yellow-500',
+  26:'bg-yellow-600',
+  27:'bg-yellow-700',
+  28:'bg-yellow-800',
+  29:'bg-yellow-900',
+  30:'bg-yellow-950',
+}
+
+
   return (
     <>
       <div className='outLine m-auto'>
-        <div className={`innerLine colorChange ${getColorClass()} `}>
+        <div className={`innerLine colorChange ${colorSelect[count]} `}>
+        {/* <div className={`innerLine colorChange ${getColorClass()} `}> */}
           <div className='valueBox m-auto mt-6 font-bold text-3xl rounded-lg bg-green'>{count}</div>
           {error && (<div className='mt-3 bg-white w-80 m-auto '> <h1 className='text-red-600'>Invalid Value</h1></div>)}
           <div className='flex justify-around mt-11'>
@@ -75,7 +110,8 @@ const Counter = () => {
           <button onClick={applyCustomValue} className='border p-1 bg-black text-white font-semibold mt-2 ml-28 px-3'>Apply</button>
           <button onClick={() => resetFunction()} className='border p-1 bg-black text-white font-semibold mt-2 ml-5 px-3'>Reset</button>
           <div className='border mt-4 p-10 bg-black'>
-            <div className={`border colorChange ${getColorClass()}`}>
+            <div className={`border colorChange ${colorSelect[count]}`}>
+            {/* <div className={`border colorChange ${getColorClass()}`}> */}
               <h1 className='font-bold p-1'>VALUE RANGES</h1>
               <div className='flex flex-col pb-4'>
                 <label>Red : 1 - 10</label>
